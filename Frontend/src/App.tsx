037b8cde58e72c81a904cd590c032219732bc4a7
CharlesCryer <161407816+CharlesCryer@users.noreply.github.com>
2024-07-26 15:14:28 +1200

    Adds mantine core and hooks

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,9 @@
-const App = () => {
-  return <div>App</div>;
-};
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
 
-export default App;
+import { MantineProvider } from "@mantine/core";
+
+export default function App() {
+  return <MantineProvider></MantineProvider>;
+}
